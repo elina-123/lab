1797c64238775fef10aa435fa1caa4334e9379f7
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-11-04 09:54:40 +0530

    Add files via upload

---
+++ 03.11.2022/ArraySpecificValue.java
@@ -0,0 +1,17 @@
+package pro1;
+//Write a Java program to test if an array contains a specific value
+public class ArraySpecificValue {
+	public static boolean contains(int[] arr, int item) {
+	      for (int n : arr) {
+	         if (item == n) {
+	            return true;
+	         }
+	      }
+	      return false;
+	   }
+	   public static void main(String[] args) {
+	          int[] my_array1 = { 1789, 2035, 1899, 1456, 2013 };
+	          
+	      System.out.println(contains(my_array1, 2013));
+	      System.out.println(contains(my_array1, 2015));
+	   }}
