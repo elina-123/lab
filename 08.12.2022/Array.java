f85ed025e714f431dacabcb3f76dd67ac1cf9698
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-12-08 14:02:59 +0530

    Add files via upload

---
+++ 08.12.2022/Array.java
@@ -0,0 +1,17 @@
+package pro1;
+import java.util.ArrayList;
+
+public class Array {
+public static void main(String[] args){
+
+System.out.println("Original Array: ");
+ArrayList<string> arr1=new ArrayList<string>();
+arr1.add("Red");
+arr1.add("Blue");
+arr1.add("White");
+arr1.add("Orange");
+System.out.println(arr1);
+System.out.println("Reversed Array: ");
+System.out.println(arr1.subList(0, 3));
+}
+}
