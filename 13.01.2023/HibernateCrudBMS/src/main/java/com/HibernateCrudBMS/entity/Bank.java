0d62b8a930d706c981e68739627a9a241e68e438
elina-123 <112066749+elina-123@users.noreply.github.com>
2023-01-13 09:58:11 +0530

    Add files via upload

---
+++ 13.01.2023/HibernateCrudBMS/src/main/java/com/HibernateCrudBMS/entity/Bank.java
@@ -0,0 +1,71 @@
+package com.HibernateCrudBMS.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="bank_Information")
+public class Bank {
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
+private int accountNo;
+@Column(length=20,nullable=false)
+private String name;
+@Column(length=20,nullable=false,unique=true)
+private String ifsccode;
+@Column(length=11,nullable=false, unique=true)
+private long phone;
+@Column(length=11,nullable=false)
+private int deposit;
+@Column(length=25,nullable=false,unique=true)
+private String email;
+public int getAccountNo() {
+	return accountNo;
+}
+public void setAccountNo(int accountNo) {
+	this.accountNo = accountNo;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getIfsccode() {
+	return ifsccode;
+}
+public void setIfsccode(String ifsccode) {
+	this.ifsccode = ifsccode;
+}
+public long getPhone() {
+	return phone;
+}
+public void setPhone(long phone) {
+	this.phone = phone;
+}
+public int getDeposit() {
+	return deposit;
+}
+public void setDeposit(int deposit) {
+	this.deposit = deposit;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+}
---
+++ 13.01.2023/HibernateCrudBMS/src/main/java/com/HibernateCrudBMS/entity/package-info.java
@@ -0,0 +1,1 @@
+package com.HibernateCrudBMS.entity;
